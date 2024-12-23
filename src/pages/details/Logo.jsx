518252add66e2ff2e35ecdccccf077dde3704f17
jaeuk-choi <104468228+jaeuk-choi@users.noreply.github.com>
2024-12-23 17:05:13 +0900

    상세페이지1

--- src/pages/details/Logo.jsx
+++ src/pages/details/Logo.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { Image, Box } from "@chakra-ui/react";
-import logo from './images/logo.png';  // import로 변경
+import { useNavigate } from "react-router-dom";
+import logo from './images/logo.png';
 
 const Logo = () => {
+  const navigate = useNavigate(); // useNavigate 훅을 사용하여 네비게이터 객체 생성
+
+  const handleLogoClick = () => {
+    navigate("/"); // 메인 페이지로 이동
+  };
+
   return (
     <Box mb={4}>
-      <Image src={logo} alt="Logo" maxH="50px" mx="left" />
+      <Image 
+        src={logo} 
+        alt="Logo" 
+        maxH="50px" 
+        mx="left" 
+        cursor="pointer" 
+        onClick={handleLogoClick}
+      />
     </Box>
   );
 };
