9d1a8e4ee3cd6abf68e59b148daf03012df953a3
rmk <qop589@naver.com>
2025-01-02 16:03:08 +0900

    fix:별점리뷰등록&마이페이지 비로그인 시 접근불가

--- src/pages/details/Tabs/StarReview/StarReviews.jsx
+++ src/pages/details/Tabs/StarReview/StarReviews.jsx
@@ -1,4 +1,5 @@
 import { Box, Button, Card, HStack } from '@chakra-ui/react';
+import { useSelector } from "react-redux";
 import React, { useEffect, useState, useRef } from 'react';
 import StarReviewCard from './StarReviewCard';
 import CustomDialog from './CustomDialog';
@@ -11,20 +12,23 @@ export default function StarReviews({no}) {
   const [memberNo, setMemberNo] = useState(0); // 상태로 관리
   const [nickName, setNickName] = useState(""); // 상태로 관리
   const refreshReviews = useRef();
+  const isLoggedIn = useSelector(state=>state.auth.isLoggedIn);
   useEffect(()=>{
-    const fetchMemberNo = async () => {
-      try {
-        // 서버에서 사용자 정보를 가져옵니다.
-        const response = await axios.get("http://localhost:8080/main/mypage/members/read", {
-          withCredentials: true, // 쿠키를 함께 전송하도록 설정
-        });
-        setMemberNo(response.data.no); // 상태를 업데이트
-        setNickName(response.data.nickname); // 상태를 업데이트
-      } catch (error) {
-        console.error("닉네임을 가져오는 데 실패했습니다:", error);
-      }
-    };
-    fetchMemberNo();
+    if(isLoggedIn){
+      const fetchMemberNo = async () => {
+        try {
+          // 서버에서 사용자 정보를 가져옵니다.
+          const response = await axios.get("http://localhost:8080/main/mypage/members/read", {
+            withCredentials: true, // 쿠키를 함께 전송하도록 설정
+          });
+          setMemberNo(response.data.no); // 상태를 업데이트
+          setNickName(response.data.nickname); // 상태를 업데이트
+        } catch (error) {
+          console.error("닉네임을 가져오는 데 실패했습니다:", error);
+        }
+      };
+      fetchMemberNo();
+    }
   }, []);
   return (
     <Card.Root w="11/12" mx="auto">
@@ -34,6 +38,7 @@ export default function StarReviews({no}) {
             <Button mr="3" onClick={() => handleSortChange("createdAt")} variant={sortBy === "createdAt" ? "subtle" : "outline"}>최신순</Button>
             <Button onClick={() => handleSortChange("rating")} variant={sortBy === "rating" ? "subtle" : "outline"}>추천순</Button>
           </Box>
+          {isLoggedIn ?
           <CustomDialog
             openBtnText="리뷰 쓰기"
             title={nickName}
@@ -43,6 +48,9 @@ export default function StarReviews({no}) {
             closeBtnText="취소"
             onReviewSubmitted={() => refreshReviews.current()}
           />
+          :
+          null
+          }
         </HStack>
       </Card.Header>
       <Card.Body>
