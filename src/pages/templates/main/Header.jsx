2e610b326e214782a02137c6bcfdb7af1247f69f
EaseHee <geonhee@naver.com>
2025-01-02 12:59:30 +0900

    fix: 헤더 아이콘 삭제

--- src/pages/templates/main/Header.jsx
+++ src/pages/templates/main/Header.jsx
@@ -6,11 +6,9 @@ import axios from "utils/axios";
 import { useTheme } from "next-themes";
 import { Box, Button, Flex, HStack, Text } from "@chakra-ui/react";
 
-import { menuItems } from "../../sidebar/Sidebar";
 import { logout } from "../../../redux/slices/authSlice";
 import Logo from "../../../components/Logo";
 import { useEffect, useState } from "react";
-import {useColorMode, useColorModeValue} from "../../../components/ui/color-mode";
 
 const Header = () => {
   // 사용자의 로그인 상태를 redux에 저장하여 useSelector와 useDispatch 훅을 이용해서 전역적으로 관리
@@ -59,31 +57,6 @@ const Header = () => {
   // 상세 페이지로 이동 시 active 속성을 제거하기 위해 useLocation 훅을 이용
   const location = useLocation();
 
-  const renderMenuItems = () => {
-    return menuItems.map((item, index) => {
-      // 현재 경로와 메뉴의 경로가 다를 경우 active 해제
-      const isActive = location.pathname === item.path;
-      return (
-        // JSX 반환
-        <Link to={item.path} key={index} style={{ textDecoration: "none" }}>
-          <Flex
-            direction="column"
-            align="center"
-            justify="center"
-            gap={2}
-            color={isActive ? "blue.600" : "gray.500"}
-            _hover={{ color: "blue.600" }}
-          >
-            <Box as="span">{item.icon}</Box>
-            <Text fontSize="sm" fontWeight={600} textAlign="center">
-              {item.label}
-            </Text>
-          </Flex>
-        </Link>
-      );
-    });
-  };
-
   return (
     <Flex
       justify="space-between"
@@ -106,16 +79,6 @@ const Header = () => {
       <Box>
         <Logo />
       </Box>
-
-      {/* 메뉴 */}
-      <HStack
-        as="nav"
-        spacing={8}
-        flexWrap="nowrap"
-        justify="space-between"
-      >
-        {renderMenuItems()}
-      </HStack>
 
       {/* 로그인/로그아웃 버튼 + 닉네임 + 테마 토글 */}
       <Flex align="center" gap={4}>
