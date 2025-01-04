f7b09c8589ecd356899af3f3f2623f4b0b21901f
EaseHee <geonhee@naver.com>
2025-01-04 22:19:39 +0900

    release: BASE_URL 환경 변수 설정

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,5 @@
 ﻿import axios from "axios";
-const baseURL = process.env.REACT_APP_API_BASE_URL;
+const baseURL = process.env.REACT_APP_API_BASE_URL || "port-0-forklog-m10lhqc01e8bd7d0.sel4.cloudtype.app";
 /**
  * 서버 요청 기본 URL 설정 및 쿠키 전송 허용 여부 설정 인스턴스 생성
  * @type {axios.AxiosInstance}
