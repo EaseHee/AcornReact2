c64fbbb55a2c0fe1e1a29f15c8a94ab7b38662b1
EaseHee <geonhee@naver.com>
2025-01-04 21:26:29 +0900

    feat: 웹소켓 서버 송수신 확인

--- src/pages/templates/main/Body.jsx
+++ src/pages/templates/main/Body.jsx
@@ -5,7 +5,7 @@ import Main from "../../main/Main";
 import DetailPage from "../../details/DetailPage";
 import MyPage from "../../mypage/MyPage";
 import OpenChat from "pages/chat/OpenChat";
-import axios from "axios";
+import axios from "utils/axios";
 import { useEffect, useState } from "react";
 import { Client } from "@stomp/stompjs";
 import SockJS from "sockjs-client";
@@ -15,24 +15,24 @@ const Body = () => {
   const [chats, setChats] = useState([]); // 채팅 내역
   const [chatNo, setChatNo] = useState(null); // 채팅방 번호
   const [stompClient, setStompClient] = useState(null); // STOMP 클라이언트
-  const userNo = Number(sessionStorage.getItem("id")); // 사용자 번호
+  const [memberNo, setMemberNo] = useState(null);
+
+  useEffect(() => {
+    axios.get("/main/mypage/members/read")
+        .then(res => res.data)
+        .then(data => setMemberNo(data.no))
+        .catch(err => console.log(err));
+  }, []);
 
   // 메시지 전송 함수
   const handleSend = () => {
     if (message.trim() !== "") {
-      const messagePayload = {
-        chatNo: chatNo,
-        content: message,
-        sendOtherUser: false,
-        userNo: userNo,
-      };
-
-      console.log("Sending message:", JSON.stringify(messagePayload));
+      console.log("Sending message:", message, memberNo);
 
       // 메시지 전송
       stompClient.publish({
-        destination: `/pub/chat/room/${chatNo}`, // 서버에 메시지 전송
-        body: JSON.stringify(messagePayload),
+        destination: `/pub/chat/message`,
+        body: JSON.stringify({ content: message, memberNo: memberNo }),
       });
 
       setMessage(""); // 메시지 초기화
@@ -40,8 +40,10 @@ const Body = () => {
   };
 
   // Enter 키를 눌렀을 때 메시지 전송
+  // 메시지 전송 함수
   const handleKeyDown = (e) => {
-    if (e.key === "Enter") {
+    if (e.key === "Enter" && !e.nativeEvent.isComposing) {
+      e.preventDefault(); // 기본 Enter 동작 방지
       handleSend();
     }
   };
@@ -50,36 +52,29 @@ const Body = () => {
   useEffect(() => {
     // 오픈 채팅방의 기존 채팅 내역 불러오기
     axios
-      .get(`/chat/user/${userNo}`)
-      .then((response) => {
-        if (response.data.chatNo) {
-          setChatNo(response.data.chatNo.no);
-        }
-        if (response.data.chats) {
-          setChats(response.data.chats);
-        }
-        if (response.data.create) {
-          setChatNo(response.data.create.no);
-        }
-      })
+      .get(`/chat/message`)
+      .then(response => response.data )
+      .then(data => {
+        console.log(data);
+        setChats(data)
+      } )
       .catch((err) => {
         console.log(err);
       });
 
     // STOMP 클라이언트 설정 및 WebSocket 연결
-    const socket = new SockJS(`http://${window.location.hostname}:8080/ws`);
+    const socket = new SockJS(`${process.env.REACT_APP_API_BASE_URL}/ws`);
     const client = new Client({
       webSocketFactory: () => socket,
       debug: (str) => console.log("STOMP Debug: ", str),
       reconnectDelay: 5000,
-      heartbeatIncoming: 4000,
-      heartbeatOutgoing: 4000,
+      heartbeatIncoming: 0,
+      heartbeatOutgoing: 0,
     });
 
     client.onConnect = () => {
-      const chatRoomId = `1_${userNo}`;
-      console.log("Connected, subscribing to:", `/sub/chat/room/${chatRoomId}`);
-      client.subscribe(`/sub/chat/room/${chatRoomId}`, (message) => {
+      console.log("Connected, subscribing to:", `/sub/chat/message`);
+      client.subscribe(`/sub/chat/message`, (message) => {
         const receivedMessage = JSON.parse(message.body);
         setChats((prevMessages) => [...prevMessages, receivedMessage]);
       });
@@ -93,7 +88,7 @@ const Body = () => {
         client.deactivate();
       }
     };
-  }, [userNo]);
+  }, []);
 
   return (
     <Flex
